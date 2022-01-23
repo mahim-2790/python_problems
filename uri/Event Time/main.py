a0544c0fee21e0711fb51ec2556adfd24ff44906
Mohi Uddin Mahim <mohi35-2790@diu.edu.bd>
2022-01-23 20:52:03 +0600

    uri evet problems

---
+++ uri/Event Time/main.py
@@ -0,0 +1,32 @@
+dia, date1 = input().split()
+date1 = int(date1)
+h1,m1,s1 = map(int,input().split(":"))
+
+
+
+dia, date2 = input().split()
+date2 = int(date2)
+h2,m2,s2 = map(int,input().split(":"))
+
+
+passesSeconds = h1*3600 + m1*60 + s1
+remainingSeconds = 86400 - passesSeconds
+remainingSeconds2 = h2*3600 + m2*60 + s2
+
+date =(date2-1)-date1
+remainDateSeconds = date*86400
+
+totalSeconds = remainingSeconds+remainingSeconds2+remainDateSeconds
+
+finalDate = totalSeconds/86400
+totalSeconds = totalSeconds%86400
+finalHour = totalSeconds/3600
+totalSeconds = totalSeconds%3600
+finalMinutes = totalSeconds/60
+totalSeconds = totalSeconds%60
+
+print("%i dia(s)"%finalDate)
+print("%i hora(s)"%finalHour)
+print("%i minuto(s)"%finalMinutes)
+print("%i segundo(s)"%totalSeconds)
+
